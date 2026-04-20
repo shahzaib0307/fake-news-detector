b2117857db5f037ba76bafc78b5980ee6ceb139a
Shahzaib Hasan <shahzaibhasan2005@gmail.com>
2026-04-20 21:43:54 +0500

    fix railway deployment (host fix)

--- app.py
+++ app.py
@@ -183,5 +183,6 @@ def stats():
 # ─────────────────────────────────────────────────
 if __name__ == '__main__':
     init_db()
-    print("Starting Flask server at http://127.0.0.1:5000")
-    app.run(debug=True, port=5000)
+    print("Starting Flask server...")
+
+    app.run(host="0.0.0.0", port=5000)
